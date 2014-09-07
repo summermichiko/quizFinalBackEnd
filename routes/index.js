db03a439fe60c3eadbb256d4df5644ec388ef57e
Summer McDonald <summermichiko@gmail.com>
2014-09-07 18:35:39 -0400

    history seed file questions

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,27 @@ var quiz = [
 		'answer': "Antarctica", 
 		'difficulty': 5
 		},
-		{ "q": "What was the first planet to be discovered using the telescope in 1781??", 
+		{ "q": "What was the first planet to be discovered using the telescope in 1781?", 
 		'options':[{ 'value': 'Mars'} , { 'value': 'Uranus'} , {'value' : 'Pluto'} , { 'value': 'none of the above'}], 
 		'answer':'Uranus',
 		'difficulty': 2
 		},
 		{ 
-		'q': "What is the diamete of Earth?", 
+		'q': "What is the diameter of Earth?", 
 		'options':[{ 'value': '5,000 miles'} ,{ 'value': '3,500 miles'} , {'value' : '10,000 miles'} , { 'value': '8,000 miles'}], 
 		'answer':'8,000 miles',
 		'difficulty': 6
 		},
 		{ 
-		'q': "Someone who suffers from oneirophobia is scared of what?", 
-		'options':[{ 'value': 'Dreams'} ,{ 'value': 'Light'} , {'value' : 'technology'} , { 'value': 'plants'}], 
-		'answer':'Dreams',
+		'q': "How many U.S. states border the Gulf of Mexico?", 
+		'options':[{ 'value': 'Four'} ,{ 'value': 'Six'} , {'value' : 'Three'} , { 'value': 'Five'}], 
+		'answer':'Five',
+		'difficulty': 1
+		},
+		{ 
+		'q': "Who, after anchoring off Hawaii in 1779, was mistaken for the god Lono?", 
+		'options':[{ 'value': 'Christopher Columbus'} ,{ 'value': 'Captain James Cook'} , {'value' : 'King Kamehameha I'} , { 'value': 'Prince Kuhio'}], 
+		'answer':'Captain James Cook',
 		'difficulty': 1
 		}
 	];
